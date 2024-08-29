e2c2f7aa65d0377f4319b9883094078ea4e5f71c
Ultimatexuma <153943073+Ultimatexuma@users.noreply.github.com>
2024-08-29 22:23:58 +0100

    Update config.js

--- config.js
+++ config.js
@@ -2,20 +2,20 @@
 
 
 require("./all/module")
-global.owner = "254742491666" 
-global.namaCreator = "𝕶𝖎𝖓𝖌 𝕾𝖆𝖒" 
-global.autoJoin = false 
-global.antilink = false 
+global.owner = "2348132534246" 
+global.namaCreator = "FATIHU" 
+global.autoJoin = "false" 
+global.antilink = "true" 
 global.versisc = '6.0' 
 global.codeInvite = "https://whatsapp.com/channel/0029VaaqaSp0LKZDuwe5SI3e"
 global.isLink = 'https://whatsapp.com/channel/0029VaaqaSp0LKZDuwe5SI3e'
 global.sessionName = 'session'
 global.thumb = fs.readFileSync("./thumb.png") 
 global.audionya = fs.readFileSync("./all/sound.mp3") 
-global.packname = "𝕶𝖎𝖓𝖌 𝕾𝖆𝖒" 
-global.author = "𝐒𝚸𝚰𝐃𝚵𝚪-𝛁6" 
+global.packname = "BOD_XUMA" 
+global.author = "🙃BOD_XUMA🙃" 
 global.jumlah = "5" 
-global.namabot = "𝐒𝚸𝚰𝐃𝚵𝚪-𝛁6" 
+global.namabot = "XUMA" 
 global.domain = 'https://kingsam.sellerpanelerol.me' // your domian
 global.apikey = 'ptla_VRxkhvalZQ3EhHyAax9QYxgT5cbsx2MrO69gBpVZmmd' // Isi Apikey Plta Lu
 global.capikey = 'ptlc_sQ3d4GxCiztyV5DbhUJSWtvnSCyKzeMXoWIr2lVyqal' // Isi Apikey Pltc Lu
